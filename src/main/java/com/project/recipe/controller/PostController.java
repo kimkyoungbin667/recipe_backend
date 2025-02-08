79d419d8a1f096ff3635744d8b20385491a58c45
kkb <rudqls1144@naver.com>
2025-02-08 14:44:50 +0900

    feat: 에러 처리 핸들러 설정

--- src/main/java/com/project/recipe/controller/PostController.java
+++ src/main/java/com/project/recipe/controller/PostController.java
@@ -1,5 +1,6 @@
 package com.project.recipe.controller;
 
+import com.project.recipe.config.CustomException;
 import com.project.recipe.entity.Post;
 import com.project.recipe.service.PostService;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -18,6 +19,14 @@ public class PostController {
     // 게시글 작성
     @PostMapping
     public ResponseEntity<Post> createPost(@RequestBody Post post) {
+
+        // 제목이 없다면
+        if(post.getTitle() == null || post.getTitle().isEmpty()) {
+            throw new CustomException("제목을 입력해주세요.");}
+        // 내용이 없다면
+        else if (post.getContent() == null || post.getContent().isEmpty()) {
+            throw new CustomException("내용을 입력해주세요.");}
+
         return ResponseEntity.ok(postService.createPost(post));
     }
 
