7361422ceff510655eb03297b302e6b7e56a16a4
kkb <rudqls1144@naver.com>
2025-02-08 17:14:38 +0900

    feat: 응답 메시지 반환을 위한 ResponseMessage DTO 추가

--- src/main/java/com/project/recipe/controller/PostController.java
+++ src/main/java/com/project/recipe/controller/PostController.java
@@ -1,9 +1,12 @@
 package com.project.recipe.controller;
 
 import com.project.recipe.config.CustomException;
+import com.project.recipe.dto.ResponseMessage;
 import com.project.recipe.entity.Post;
 import com.project.recipe.service.PostService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
@@ -18,17 +21,30 @@ public class PostController {
 
     // 게시글 작성
     @PostMapping
-    public ResponseEntity<Post> createPost(@RequestBody Post post) {
+    public ResponseEntity<?> createPost(@RequestBody Post post) {
 
-        // 제목이 없다면
+        // 작성자 번호가 없을 때
+        if(post.getUserNo() == null) {
+            throw new CustomException("작성자 번호가 없습니다.");}
+        // 제목이 없을 때
         if(post.getTitle() == null || post.getTitle().isEmpty()) {
-            throw new CustomException("제목을 입력해주세요.");}
-        // 내용이 없다면
+            throw new CustomException("제목이 없습니다.");}
+        // 내용이 없을 때
         else if (post.getContent() == null || post.getContent().isEmpty()) {
-            throw new CustomException("내용을 입력해주세요.");}
-
-        return ResponseEntity.ok(postService.createPost(post));
+            throw new CustomException("내용이 없습니다.");}
+        // 카테고리가 없을 때
+        else if (post.getCategory() == null || post.getCategory().isEmpty()) {
+            throw new CustomException("카테고리가 없습니다.");}
+        return ResponseEntity.ok(new ResponseMessage(200, "게시글이 성공적으로 등록되었습니다.", postService.createPost(post)));
     }
+
+    // 게시글 전체 조회
+    @GetMapping
+    public ResponseEntity<Page<Post>> getAllPosts(Pageable pageable) {
+
+        return ResponseEntity.ok(postService.findAllPosts(pageable));
+    }
+
 
 
 }
--- src/main/java/com/project/recipe/service/PostService.java
+++ src/main/java/com/project/recipe/service/PostService.java
@@ -3,6 +3,8 @@ package com.project.recipe.service;
 import com.project.recipe.entity.Post;
 import com.project.recipe.repository.PostRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
@@ -16,6 +18,11 @@ public class PostService {
     // 게시글 작성
     public Post createPost(Post post) {
         return postRepository.save(post);
+    }
+
+    // 게시글 전체 조회
+    public Page<Post> findAllPosts(Pageable pageable) {
+        return postRepository.findAll(pageable);
     }
 
 
